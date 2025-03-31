bdc372c71670c551c7499d753d4466154ad9f217
Koryun <koryun468@gmail.com>
2025-03-31 12:18:22 +0400

    Update Results.jsx

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -110,7 +110,7 @@ export default function Results() {
       </div>
     </div>
   ) : (
-    <div className="w-full md:min-h-[calc(100vh-78px)] text-center min-h-[calc(100vh-108px)] dark:text-gray-300 text-gray-700 gap-12 text-6xl bg-purple-300 transition-all duration-200 dark:bg-indigo-500 justify-center flex flex-col items-center">
+    <div className="w-full md:min-h-[calc(100vh-78px)] text-center min-h-[calc(100vh-108px)] dark:text-gray-300 text-gray-700 gap-12 text-4xl md:text-6xl bg-purple-300 transition-all duration-200 dark:bg-indigo-500 justify-center flex flex-col items-center">
       <p>{t("no_results")}</p>
       <StartBtn />
     </div>
